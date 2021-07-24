c86d577bb130355bcc9aa7dbe898d2a0b6991b88
kiyegga <kiyeggafrancis1@gmail.com>
2021-07-24 14:44:17 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam nihil
+        quaerat repellat. Distinctio eveniet atque cupiditate quibusdam
+        dignissimos sed quo et vero beatae, dolorum nam, porro suscipit earum
+        quis necessitatibus pariatur provident quam excepturi voluptas? Saepe
+        deserunt molestias libero non!
+      </p>
+    </section>
+  );
+};
+
+export default About;
